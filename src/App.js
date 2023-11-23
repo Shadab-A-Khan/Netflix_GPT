de1a8c7ef9552e54b9bc373c1d4cbafd595fb107
Shadab Ahmad Khan <shadabahmadkhan4@gmail.com>
2023-11-24 00:16:10 +0530

    tailwind ,ssh-key , github repo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-         <h1>Hellow.. Hellow.. Hellow.. </h1>
+        <h1 className=" flex m-auto text-4xl text-blue-500 font-bold justify-center">
+          Hellow.. Hellow.. Hellow..{" "}
+        </h1>
       </header>
     </div>
   );
