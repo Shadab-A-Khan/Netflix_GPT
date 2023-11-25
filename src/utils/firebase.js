acb604b89ef1f4689f45694ce9d1a0e4f3da1eb9
Shadab Ahmad Khan <shadabahmadkhan4@gmail.com>
2023-11-25 20:18:59 +0530

    complete firebase set-up

---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBEvOtQxB8CBiDx95SktLhlbeYpMTCea9o",
+  authDomain: "netflixgpt-f1701.firebaseapp.com",
+  projectId: "netflixgpt-f1701",
+  storageBucket: "netflixgpt-f1701.appspot.com",
+  messagingSenderId: "362814768343",
+  appId: "1:362814768343:web:8b4815dfea4832b8161c92",
+  measurementId: "G-P5DWX5PZRH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);npm install -g firebase-tools
--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,11 +1,14 @@
-export const checkValidData = (email, password,name) => {
+export const checkValidData = (email, password, name) => {
   const isEmailValid = /[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
+
   const isPasswordValid =
     /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
 
   const isValidUserName = /([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+/.test(name);
+
   if (!isValidUserName) return "User Name is not valid";
   if (!isEmailValid) return "Email ID is not valid";
   if (!isPasswordValid) return "Password is not valid";
+
   return null;
 };
